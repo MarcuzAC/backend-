f78abaf2297ea7b8c10ff1ca18183c357d03c9aa
Milton Chithenga <miltonachithenga@gmail.com>
2025-03-02 11:06:56 +0200

    chanGEd  database

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from config import settings
 
 # Initialize MongoDB connection
-client = AsyncIOMotorClient(settings.MONGO_URI)
+client = AsyncIOMotorClient(settings.DATABASE_URL)
 db = client.get_database()  
 
 async def get_db():
