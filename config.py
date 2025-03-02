ba15140d78db4e5f58c3e4b6da35b19cf94c9a3b
Milton Chithenga <miltonachithenga@gmail.com>
2025-03-02 10:52:56 +0200

    chanhed  database

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ class Settings:
     VIMEO_CLIENT_ID = os.getenv("VIMEO_CLIENT_ID")
     VIMEO_CLIENT_SECRET = os.getenv("VIMEO_CLIENT_SECRET")
     VIMEO_ACCESS_TOKEN = os.getenv("VIMEO_ACCESS_TOKEN")
-    #DATABASE_URL = os.getenv("DATABASE_URL")
+    DATABASE_URL = os.getenv("DATABASE_URL")
 
 settings = Settings()
