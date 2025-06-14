2f4c33b7423c6f39f5b99738eafdb46ef5b61df5
Milton Chithenga <88409286+MarcuzAC@users.noreply.github.com>
2025-06-14 15:38:08 +0200

    Initialization

--- routers/__init__.py
+++ routers/__init__.py
@@ -2,3 +2,6 @@
 from .users import router as users_router
 from .videos import router as videos_router
 from .categories import router as categories_router
+from .comments import router as comments_router
+from .likes import router as likes_router
+from .news import router as news_router
